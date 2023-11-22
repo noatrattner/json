6b6a3601c239442b4b3af29771c11931d2b54d30
noatrattner <yasharofn@gmail.com>
2023-11-22 20:07:05 +0200

    implanted simple reading

--- main.c
+++ main.c
@@ -2,6 +2,22 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("hello world\n");
+    if (argc < 2)
+    {
+        printf("usage: %s <json path>\n", argv[0]);
+        return 1;
+    }
+
+    // open file
+    const char *file_path = argv[1];
+    FILE *file = fopen(file_path, "r");
+
+    // read all the file
+    int next;
+    for (int next = getc(file); next != EOF; next = getc(file))
+    {
+        printf("%c", next);
+    }
+
     return 0;
 }
